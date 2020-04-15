ce1904c8805de324d33ed8e10a89e32e0bf0a842
brisi <brisilda.munguli@thoughtworks.com>
2020-04-15 16:09:38 +0200

    Ana, Brisi - testing props passing on CodeBreakerAttempt

--- ui/src/components/CodeBreakerAttempt.test.js
+++ ui/src/components/CodeBreakerAttempt.test.js
@@ -5,16 +5,28 @@ import CodeBreakerAttempt from './CodeBreakerAttempt';
 import attempt from "./mock/attempt";
 
 describe("<CodeBreakerAttempt />", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<CodeBreakerAttempt attempt={attempt}/>);
+    })
 
     it("should be true", () => {
         expect(true).toBe(true);
     });
 
     it("should render <Peg/> 4 times", () => {
-        const wrapper = shallow(<CodeBreakerAttempt attempt={attempt}/>);
         const pegs = wrapper.find(Peg);
-
         expect(pegs).toHaveLength(4);
     });
+
+    it("should pass attemptColor to the peg", () => {
+        const pegs = wrapper.find(Peg);
+        let index = 0;
+        pegs.forEach(peg => {
+            expect(peg.prop('className')).toEqual(attempt[index].color);
+            index++;
+        })
+    })
 
 })
