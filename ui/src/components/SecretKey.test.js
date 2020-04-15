77a00a2de51f6eb3d4ff3e9fc44e2431263c9c9f
brisi <brisilda.munguli@thoughtworks.com>
2020-04-15 17:03:01 +0200

    Ana, Brisi - testing props on SecretKey

--- ui/src/components/SecretKey.test.js
+++ ui/src/components/SecretKey.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import {shallow} from 'enzyme';
 import Peg from "./Peg";
 import SecretKey from './SecretKey';
+import feedback from "./mock/feedback";
 
 describe("<SecretKey />", () => {
 
@@ -15,5 +16,18 @@ describe("<SecretKey />", () => {
 
         expect(pegs).toHaveLength(4);
     });
+
+    it("should pass className to each Peg", () => {
+        const secretKeyClassName = "btn btn-dark btn-circle btn-xl border border-dark";
+        const wrapper = shallow(<SecretKey className={secretKeyClassName}/>);
+        const pegs = wrapper.find(Peg);
+
+        let index = 0;
+        pegs.forEach(peg => {
+            expect(peg.prop('className')).toEqual(secretKeyClassName);
+            index++;
+        })
+
+    })
 
 })
