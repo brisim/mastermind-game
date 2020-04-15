ee019a27dc6306a1d67c875c01eeeddb2d46e66d
brisi <brisilda.munguli@thoughtworks.com>
2020-04-15 16:45:04 +0200

    Ana, Brisi - testing props on Row

--- ui/src/components/Row.test.js
+++ ui/src/components/Row.test.js
@@ -3,18 +3,39 @@ import {shallow} from "enzyme";
 import CodeMakerFeedback from "./CodeMakerFeedback";
 import CodeBreakerAttempt from "./CodeBreakerAttempt";
 import Row from "./Row";
+import attempt from "./mock/attempt";
+import feedback from "./mock/feedback";
 
 describe("<Row/>", () => {
     it("should pass", () => {
         expect(true).toBe(true);
     });
 
-    it("should have CodeMakerFeedback and CodeBreakerAttempt", () => {
+    it("should have CodeMakerFeedback", () => {
         const wrapper = shallow(<Row/>);
         const feedbackRow = wrapper.find(CodeMakerFeedback);
-        const pegList = wrapper.find(CodeBreakerAttempt);
 
         expect(feedbackRow.exists()).toBe(true);
-        expect(pegList.exists()).toBe(true);
+    })
+
+    it("should pass feedback prop to CodeMakerFeedback", () => {
+        const wrapper = shallow(<Row feedback={feedback}/>);
+        const feedbackRow = wrapper.find(CodeMakerFeedback);
+
+        expect(feedbackRow.prop("feedback")).toEqual(feedback);
+    })
+
+    it("should have CodeBreakerAttempt", () => {
+        const wrapper = shallow(<Row/>);
+        const attemptRow = wrapper.find(CodeBreakerAttempt);
+
+        expect(attemptRow.exists()).toBe(true);
+    })
+
+    it("should pass attempt prop to CodeBreakerAttempt", () => {
+        const wrapper = shallow(<Row attempt={attempt}/>);
+        const attemptRow = wrapper.find(CodeBreakerAttempt);
+
+        expect(attemptRow.prop("attempt")).toEqual(attempt);
     })
 })
