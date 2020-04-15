7bd979889bd384969948f62a9db2270c8058aced
brisi <brisilda.munguli@thoughtworks.com>
2020-04-15 16:01:18 +0200

    Ana, Brisi - testing pros on Peg

--- ui/src/components/Peg.test.js
+++ ui/src/components/Peg.test.js
@@ -13,4 +13,20 @@ describe("<Peg/>", () => {
         const button = wrapper.find('button');
         expect(button.exists()).toBe(true);
     })
+
+    it("should pass className prop to the button", () => {
+        const className = "btn btn-success btn-circle btn-xl";
+        const wrapper = shallow(<Peg className={className}/>);
+        const button = wrapper.find('button');
+
+        expect(button.prop('className')).toEqual(className);
+    });
+
+    it("should pass changeColor prop to the button", () => {
+        const changeColor = jest.fn();
+        const wrapper = shallow(<Peg changeColor={changeColor}/>);
+        const button = wrapper.find('button');
+
+        expect(button.prop('onClick')).toEqual(changeColor);
+    })
 })
