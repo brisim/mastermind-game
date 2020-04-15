aff4cb88173e6bcf64aeb16e1da8c4b1945cd31c
brisi <brisilda.munguli@thoughtworks.com>
2020-04-15 16:14:58 +0200

    testing props on CodeMakerFeedback

--- ui/src/components/CodeMakerFeedback.test.js
+++ ui/src/components/CodeMakerFeedback.test.js
@@ -3,16 +3,32 @@ import {shallow} from "enzyme";
 import CodeMakerFeedback from "./CodeMakerFeedback";
 import Peg from "./Peg";
 import feedback from "./mock/feedback";
+import attempt from "./mock/attempt";
+import CodeBreakerAttempt from "./CodeBreakerAttempt";
 
 describe("<CodeMakerFeedback/>", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<CodeMakerFeedback feedback={feedback}/>);
+    })
+
     it("should pass", () => {
         expect(true).toBe(true);
     });
 
     it("should render <Peg/> 4 times", () => {
-        const wrapper = shallow(<CodeMakerFeedback feedback={feedback}/>);
         const pegs = wrapper.find(Peg);
 
         expect(pegs).toHaveLength(4);
     });
+
+    it("should pass feedback to the peg", () => {
+        const pegs = wrapper.find(Peg);
+        let index = 0;
+        pegs.forEach(peg => {
+            expect(peg.prop('className')).toEqual(feedback[index].color);
+            index++;
+        })
+    })
 })
