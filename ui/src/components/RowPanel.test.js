0b58cccd69fd083f9057a5d68ba806504b40724d
brisi <brisilda.munguli@thoughtworks.com>
2020-04-15 16:56:02 +0200

    Ana, Brisi - testing props on RowPanel

--- ui/src/components/RowPanel.test.js
+++ ui/src/components/RowPanel.test.js
@@ -2,19 +2,46 @@ import React from "react";
 import {shallow} from 'enzyme';
 import RowPanel from "./RowPanel";
 import Row from "./Row";
+import feedback from "./mock/feedback";
 
 describe("<RowPanel/>", () => {
+    let wrapper;
+    let rowList;
+
+    beforeEach(() => {
+        const rowConfiguration = {attempt:[], feedback:[]};
+        rowList = new Array(10);
+        rowList.fill(rowConfiguration);
+        wrapper = shallow(<RowPanel rows={rowList}/>);
+    })
+
     it("should pass", () => {
         expect(true).toBe(true);
     });
 
     it("should render 10 <Row/>", () => {
-        const rowConfiguration = {attempt:[], feedback:[]};
-        const rowList = new Array(10);
-        rowList.fill(rowConfiguration);
-        const wrapper = shallow(<RowPanel rows={rowList}/>);
         const rows = wrapper.find(Row);
 
         expect(rows).toHaveLength(10);
+    })
+
+    it("should pass attempt to each Row", () => {
+        const rows = wrapper.find(Row);
+
+        let index = 0;
+        rows.forEach(row => {
+            expect(row.prop('attempt')).toEqual(rowList[index].attempt);
+            index++;
+        })
+    })
+
+    it("should pass feedback to each Row", () => {
+        const rows = wrapper.find(Row);
+
+        let index = 0;
+        rows.forEach(row => {
+            expect(row.prop('feedback')).toEqual(rowList[index].feedback);
+            index++;
+        })
     })
 })
