16eed27aa7483dfcb87acb55dd2cd4d98af3eeb4
brisi <brisilda.munguli@thoughtworks.com>
2020-05-10 20:00:12 +0200

    Brisi- add colors as map

---
+++ ui/src/components/data/attemptColors.js
@@ -0,0 +1,11 @@
+const attemptColors = new Map([]);
+
+attemptColors.set("WHITE", "btn btn-white btn-circle btn-xl border border-dark")
+.set("BLUE", "btn btn-primary btn-circle btn-xl")
+.set("GREEN", "btn btn-success btn-circle btn-xl")
+.set("OPEN_BLUE", "btn btn-info btn-circle btn-xl")
+.set("ORANGE", "btn btn-warning btn-circle btn-xl")
+.set("RED", "btn btn-danger btn-circle btn-xl")
+.set("GREY", "btn btn-secondary btn-circle btn-xl");
+
+export default attemptColors;
